833b6ad1dc28bb74d8dd264a676e47d4c547f9c8
guillochon <guillochon@gmail.com>
2016-10-13 09:25:30 -0400

    MAINT: removed pyc files

--- gcnlive/main.py
+++ gcnlive/main.py
@@ -1,4 +1,15 @@
+import os
+
 import twitter
 
+
 def main():
-    pass
+    key_path = os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), '.keysecret')
+    with open(key_path, 'r') as f:
+        keys = f.read().splitlines()
+    api = twitter.Api(consumer_key=keys[0],
+                      consumer_secret=keys[1],
+                      access_token_key=keys[2],
+                      access_token_secret=keys[3])
+    api.PostUpdate('My first tweet!')
